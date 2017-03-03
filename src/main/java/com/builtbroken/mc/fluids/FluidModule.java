4dfa8ef55402d09746cb66a9376751622b0ceee8
Robert Seifert <rseifert.phone@gmail.com>
2017-03-02 23:59:00 -0500

    Init

---
+++ src/main/java/com/builtbroken/mc/fluids/FluidModule.java
@@ -0,0 +1,11 @@
+package com.builtbroken.mc.fluids;
+
+/**
+ * Module class for handling all interaction with fluids for Voltz Engine and it's sub mods
+ *
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 3/2/2017.
+ */
+public class FluidModule
+{
+}
