4d3b5e9465a2b818f7a3052c6b4b686569a1aea0
Robert WC Seifert <rseifert.phone@gmail.com>
2019-02-24 19:50:30 -0500

    Fixed cap not saving

--- src/main/java/com/builtbroken/mc/fluids/bucket/cap/BucketCapProvider.java
+++ src/main/java/com/builtbroken/mc/fluids/bucket/cap/BucketCapProvider.java
@@ -3,9 +3,11 @@ package com.builtbroken.mc.fluids.bucket.cap;
 import com.builtbroken.mc.fluids.FluidModule;
 import com.builtbroken.mc.fluids.api.cap.IBucketCap;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.capabilities.ICapabilitySerializable;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 import net.minecraftforge.fluids.capability.IFluidHandlerItem;
 
@@ -14,7 +16,7 @@ import javax.annotation.Nullable;
 /**
  * Created by Dark(DarkGuardsman, Robert) on 2/24/2019.
  */
-public class BucketCapProvider implements ICapabilityProvider
+public class BucketCapProvider implements ICapabilityProvider, ICapabilitySerializable<NBTTagCompound>
 {
     final IFluidHandlerItem fluid_cap;
     final IBucketCap bucket_cap;
@@ -24,7 +26,7 @@ public class BucketCapProvider implements ICapabilityProvider
     public BucketCapProvider(ItemStack host)
     {
         this.host = host;
-        fluid_cap = new FluidCapabilityBucketWrapper(host);
+        fluid_cap = new FluidCapabilityBucketWrapper(host); //Saves directly to Stack, TODO look to see if we can change this
         bucket_cap = new CapabilityBucket(host);
     }
 
@@ -47,5 +49,22 @@ public class BucketCapProvider implements ICapabilityProvider
             return (T) fluid_cap;
         }
         return null;
+    }
+
+    @Override
+    public NBTTagCompound serializeNBT()
+    {
+        NBTTagCompound tagCompound = new NBTTagCompound();
+        tagCompound.setTag("bucket_cap", bucket_cap.save(new NBTTagCompound()));
+        return tagCompound;
+    }
+
+    @Override
+    public void deserializeNBT(NBTTagCompound nbt)
+    {
+        if (nbt.hasKey("bucket_cap"))
+        {
+            bucket_cap.load(nbt.getCompoundTag("bucket_cap"));
+        }
     }
 }
